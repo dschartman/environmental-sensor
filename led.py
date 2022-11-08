3da5b2dbf387ed95c6ccb060c4d360ce3e3b5ca3
Donald Schartman <schartmand@gmail.com>
2022-11-08 08:13:42 -0600

    New sensor (#2)
    * new sensor
    
    * Updated config to load from file

--- led.py
+++ led.py
@@ -1,16 +1,17 @@
 from machine import Pin
-from config import LED_STATUS
 
+from config import SENSOR_LED_STATUS
 
 led = Pin("LED", Pin.OUT)
 
+
 def led_status(func):
     def _wrapper(*args, **kwargs):
-        if LED_STATUS:
+        if SENSOR_LED_STATUS:
             led.on()
         data = func(*args, **kwargs)
         led.off()
-        
+
         return data
-    
+
     return _wrapper
